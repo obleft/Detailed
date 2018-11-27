f96fa2bb80e3575b334578e3579c0f32bedae7fa
Ben Hakes <bhhakes@gmail.com>
2018-11-27 17:14:18 -0600

    build model

--- Detailed/Detailed/Model.swift
+++ Detailed/Detailed/Model.swift
@@ -7,3 +7,59 @@
 //
 
 import Foundation
+import UIKit
+
+extension Int {
+    fileprivate func stringPadded(to count: Int) -> String {
+        var string = String(self)
+        while string.count < count { string = "0" + string }
+        return string
+    }
+}
+
+class Model {
+    static let shared = Model()
+    private init() {}
+    
+    enum Animation: String, CaseIterable {
+        case bureaucrat = "Talking Bureaucrat"
+        case frog = "Talking Frog"
+        case ranger = "Talking Ranger"
+        case vendor = "Talking Vendor"
+        case bot = "Talking Woodbot"
+    }
+    
+    private let cellCounts = [Animation.bureaucrat: 26, .frog: 62, .ranger: 86, .vendor: 36, .bot: 69]
+    
+    var characters: [Animation] {
+        return Animation.allCases
+    }
+    
+    func image(for character: Animation) -> UIImage {
+        // RETURN THE 000 IMAGE FOR THE GIVEN CHARACTER HERE
+        
+        let imgTitle = "\(character.rawValue)000"
+        guard let image = UIImage(named: imgTitle) else {fatalError("did not return character")}
+        return image
+    }
+    
+    func cells(for character: Animation) -> [UIImage] {
+        // RETURN AN ARRAY OF IMAGES FOR THE GIVEN CHARACTER HERE
+        var tempImgArray: [UIImage] = []
+        
+        guard let pictureNumber = cellCounts[character] else {fatalError("bad reference to cellCounts key")}
+        
+        for count in 0..<pictureNumber {
+            
+            if(count < 10){
+                guard let tempImage = UIImage.init(named:"\(character.rawValue)00\(count)") else {fatalError()}
+                tempImgArray.append(tempImage)
+            } else {
+                guard let tempImage = UIImage.init(named:"\(character.rawValue)0\(count)") else {fatalError()}
+                tempImgArray.append(tempImage)
+            }
+        }
+        
+        return tempImgArray
+    }
+}
